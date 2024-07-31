19b98aca755b587367ebbed10d52888ac5dc4586
Nogal1 <mcclendon.logan1234@gmail.com>
2024-07-31 11:42:31 -0700

    added comments and adjusted color hightlight

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 	'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant',
 	'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+	// Takes a str and conver to lowercase, filter through array keeping fruits that include input val, return filtered results.
 function search(str) {
 	let results = [];
 	const val = str.toLowerCase();
@@ -18,19 +19,23 @@ function search(str) {
 	return results;
 }
 
+	// Calls search on the input value, then calls showSuggestions with the returned results and input value.
 function searchHandler(e) {
 	const inputVal = e.currentTarget.value;
 	const results = search(inputVal);
 	showSuggestions(results, inputVal);
 }
 
+
+// Maps over results and uses regular expression to highlight matching parts of the suggestion. Populates suggestion with mapped list.
+	// Add event listener to each item for hover effect
 function showSuggestions(results, inputVal) {
 	suggestions.innerHTML = '';
 
 	if (inputVal) {
 		const suggestionsList = results.map(result => {
-			const regex = new RegExp(`(${inputVal})`, 'gi');
-			const highlighted = result.replace(regex, '<strong>$1</strong>');
+			const regex = new RegExp(`(${inputVal})`, 'gi'); //g stands for global, has regex search entire str and 'i' stands for case insenstitive
+			const highlighted = result.replace(regex, '<strong>$1</strong>'); // bolds matching char from input str and suggestions
 			return `<li>${highlighted}</li>`;
 		}).join('');
 		suggestions.innerHTML = suggestionsList;
@@ -46,19 +51,23 @@ function showSuggestions(results, inputVal) {
 	}
 }
 
+	// When suggestion is clicked, sets the input field val to clicked suggestion then clears them and removes class.
 function useSuggestion(e) {
 	input.value = e.target.innerText;
 	suggestions.innerHTML = '';
 	suggestions.classList.remove('has-suggestions');
 }
 
+	// on hover over suggestion, highlights by adding background color
 function highlightSuggestion(e) {
-	e.target.style.backgroundColor = '#ddd';
+	e.target.style.backgroundColor = '#ff9d00';
 }
 
+	// removes highlight when changing hover target
 function removeHighlight(e) {
 	e.target.style.backgroundColor = '';
 }
 
+	// add event listener to input field and suggestion lists
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
